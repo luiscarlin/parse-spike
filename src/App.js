84ee3f87c92be509f7dd75e776f005abd43880cb
Luis Carlin <luiscarlin@gmail.com>
2020-01-13 16:21:07 -0500

    add reports

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Dropzone from "./Dropzone";
 
 const App = () => (
   <main>
-    <h1>File Uploader</h1>
+    <h1>Super Duper Report Generator</h1>
+    <h2>1. Upload your files...</h2>
     <Dropzone />
+
+    <h2>2. Generate your report...</h2>
+    <button>Average Report</button>
+    <button>Colbert Report</button>
+    <button>TPS Report</button>
   </main>
 );
 
