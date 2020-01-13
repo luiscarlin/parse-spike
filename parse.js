bb51a3aa925a9bb0c6abc9cdbe526c7308585b0e
Luis Carlin <luiscarlin@gmail.com>
2020-01-13 10:55:53 -0500

    loop over values and convert to csv file

--- parse.js
+++ parse.js
@@ -1,5 +1,12 @@
-const xlsx = require("node-xlsx");
+// loop over all rows
+const nodeXlsx = require("node-xlsx");
+const workSheetsFromFile = nodeXlsx.parse(`${__dirname}/test.xlsx`);
+workSheetsFromFile[0].data.forEach((row, rowIndex) => {
+  console.log(`${rowIndex} row => `);
+  row.forEach((item, itemIndex) => console.log(`${itemIndex} ${item}`));
+});
 
-const workSheetsFromFile = xlsx.parse(`${__dirname}/test.xlsx`);
-
-console.log("hell0");
+// create a csv file from excel
+const xlsx = require("xlsx");
+const workBook = xlsx.readFile(`${__dirname}/test.xlsx`);
+xlsx.writeFile(workBook, "converted.csv", { bookType: "csv" });
