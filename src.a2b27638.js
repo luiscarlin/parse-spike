52a640face1b588c0c6775d93d4779c2c938bb52
Luis Carlin <luiscarlin@gmail.com>
2020-01-13 16:34:28 -0500

    Updates

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -34652,7 +34652,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49224" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56431" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
