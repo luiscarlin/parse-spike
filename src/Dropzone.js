4d71097c52054e66d62c9b21afa38baf5faa3d2d
Luis Carlin <luiscarlin@gmail.com>
2020-01-14 15:32:09 -0500

    remove console.logs

--- src/Dropzone.js
+++ src/Dropzone.js
@@ -1,57 +1,26 @@
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
-import "./dropzone.css";
 import XLSX from "xlsx";
+import "./dropzone.css";
 
-// const Dropzone = props => {
-//   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
-
-//   const files = acceptedFiles.map(file => (
-//     <li key={file.path}>
-//       {file.path} - {file.size} bytes
-//     </li>
-//   ));
-
-//   return (
-//     <section>
-//       <div {...getRootProps({ className: "dropzone" })}>
-//         <input {...getInputProps()} />
-//         <p>Drag 'n' drop some files here, or click to select files</p>
-//       </div>
-//       <aside>
-//         <h4>Files</h4>
-//         <ul>{files}</ul>
-//       </aside>
-//     </section>
-//   );
-// };
-
-function Dropzone() {
+const Dropzone = () => {
   const onDrop = useCallback(acceptedFiles => {
     acceptedFiles.forEach(file => {
       const reader = new FileReader();
 
       reader.onabort = () => console.log("file reading was aborted");
       reader.onerror = () => console.log("file reading has failed");
-      reader.onload = e => {
-        // Do whatever you want with the file contents
-        // const data = reader.result;
-        var data = e.target.result;
-        // console.log(binaryStr);
-
-        var workbook = XLSX.read(data, {
+      reader.onload = () => {
+        const data = reader.result;
+        const workbook = XLSX.read(data, {
           type: "binary"
         });
 
-        workbook.SheetNames.forEach(function(sheetName) {
-          // Here is your object
-          var allRows = XLSX.utils.sheet_to_row_object_array(
+        workbook.SheetNames.forEach(sheetName => {
+          const allRows = XLSX.utils.sheet_to_row_object_array(
             workbook.Sheets[sheetName]
           );
-          console.log(allRows);
-
           const allNumOnes = allRows.map(row => row.num1);
-          console.log(allNumOnes);
           const sum = allNumOnes.reduce(
             (previous, current) => previous + current
           );
@@ -70,6 +39,6 @@ function Dropzone() {
       <p>Drag 'n' drop some files here, or click to select files</p>
     </div>
   );
-}
+};
 
 export default Dropzone;
